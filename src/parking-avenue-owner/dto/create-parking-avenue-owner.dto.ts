8a11b398d87d055b364dc34756d70dc33965a411
abenidek <abenezerd422@gmail.com>
2026-04-04 10:10:49 +0300

    fix: update create parking avenue dto

--- src/parking-avenue-owner/dto/create-parking-avenue-owner.dto.ts
+++ src/parking-avenue-owner/dto/create-parking-avenue-owner.dto.ts
@@ -48,7 +48,6 @@ export class CreateParkingAvenueOwnerDto {
         minLength: 8,
     })
     @IsString()
-    @IsNotEmpty()
     @MinLength(8, { message: 'Password must be at least 8 characters long' })
     password: string;
 
